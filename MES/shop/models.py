39564410e42000d03d683b76280927bf41a330f7
mezba9970 <mezba2014@gmail.com>
2020-07-16 00:34:31 +0600

    add order model show items in checkout page

--- MES/shop/models.py
+++ MES/shop/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=30)
@@ -9,11 +11,11 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
     pub_date = models.DateField()
-    image = models.ImageField(upload_to="shop/images",default="")
-
+    image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
         return self.product_name
+
 
 class Contact(models.Model):
     msg_id = models.AutoField(primary_key=True)
@@ -25,3 +27,17 @@ class Contact(models.Model):
     def __str__(self):
         return self.name
 
+
+class Orders(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=5000)
+    name = models.CharField(max_length=111)
+    email = models.CharField(max_length=111)
+    address = models.CharField(max_length=111)
+    city = models.CharField(max_length=111)
+    state = models.CharField(max_length=111)
+    zip_code = models.CharField(max_length=111)
+    phone = models.CharField(max_length=111)
+
+    def __str__(self):
+        return self.name
