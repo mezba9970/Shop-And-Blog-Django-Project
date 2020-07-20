2472c28c993852521939bba0830c337dcdc90510
mezba9970 <mezba2014@gmail.com>
2020-07-21 01:20:53 +0600

    Order Tracker show and new model orderupdate crate

--- MES/shop/views.py
+++ MES/shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product, Contact, Orders
+from .models import Product, Contact, Orders, OrderUpdate
 from math import ceil
+import json
 
 # Create your views here.
 
@@ -33,7 +34,7 @@ def about(request):
 def contact(request):
     if request.method == 'POST':
         name = request.POST.get('name', '')
-        email = request.POST.get('email')
+        email = request.POST.get('email', '')
         phone = request.POST.get('phone', '')
         desc = request.POST.get('desc', '')
 
@@ -43,6 +44,23 @@ def contact(request):
 
 
 def tracker(request):
+    if request.method == 'POST':
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append(
+                        {'text': item.update_desc, 'time': item.timestamp.strftime("%d %B, %Y")})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
     return render(request, 'shop/tracker.html')
 
 
@@ -70,6 +88,9 @@ def checkout(request):
         order = Orders(items_json=items_json, name=name, email=email,
                        address=address, city=city, state=state, zip_code=zip_code, phone=phone)
         order.save()
+        update = OrderUpdate(order_id=order.order_id,
+                             update_desc='The order has been placed')
+        update.save()
         thank = True
         id = order.order_id
         return render(request, 'shop/checkout.html', {'thank': thank, 'id': id})
