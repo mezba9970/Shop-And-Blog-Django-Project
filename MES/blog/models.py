197c6d9cc7d6646b58615345b63f188f2c896332
mezba9970 <mezba2014@gmail.com>
2020-08-09 04:56:06 +0600

    add blog model and view

--- MES/blog/models.py
+++ MES/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Blogpost(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    heah0 = models.CharField(max_length=500)
+    cheah0 = models.CharField(max_length=5000)
+    heah1 = models.CharField(max_length=500)
+    cheah1 = models.CharField(max_length=5000)
+    heah2 = models.CharField(max_length=500)
+    cheah2 = models.CharField(max_length=5000)
+    pub_date = models.DateField()
+    thumbnail = models.ImageField(upload_to='shop/images')
+
+    def __str__(self):
+        return self.title
+    
